a4acddf4c9ab84e74b50e9d18dc2f924f65cb9b3
Valentin Valls <valentin.valls@esrf.fr>
2019-12-12 19:15:13 +0100

    Create a View getter in the plot

--- silx/gui/plot/PlotWidget.py
+++ silx/gui/plot/PlotWidget.py
@@ -67,6 +67,7 @@ from . import _utils
 from . import items
 from .items.curve import CurveStyle
 from .items.axis import TickMode  # noqa
+from .items.axis import View
 
 from .. import qt
 from ._utils.panzoom import ViewConstraints
@@ -206,6 +207,7 @@ class PlotWidget(qt.QMainWindow):
         self.__muteActiveItemChanged = False
 
         self._panWithArrowKeys = True
+        self._view = None
         self._viewConstrains = None
 
         super(PlotWidget, self).__init__(parent)
@@ -370,6 +372,15 @@ class PlotWidget(qt.QMainWindow):
                         or a :class:`BackendBase.BackendBase` class
         """
         silx.config.DEFAULT_PLOT_BACKEND = backend
+
+    def getView(self):
+        """Returns an object that represent the data view visualized by the plot.
+
+        :rtype: View
+        """
+        if self._view is None:
+            self._view = View(self)
+        return self._view
 
     def _getDirtyPlot(self):
         """Return the plot dirty flag.
