cd895f95577a1f2047261637ac2dcd4cf8220348
Valentin Valls <valentin.valls@esrf.fr>
2021-03-09 15:42:33 +0100

    Implement a sigMouseLeft

--- silx/gui/plot/PlotWidget.py
+++ silx/gui/plot/PlotWidget.py
@@ -58,6 +58,7 @@ except ImportError:
     _logger.debug("matplotlib not available")
 
 import six
+import weakref
 from ..colors import Colormap
 from .. import colors
 from . import PlotInteraction
@@ -244,6 +245,22 @@ class _PlotWidgetSelection(qt.QObject):
         """Handle active scatter change"""
         self.__activeItemChanged('scatter', previous, current)
 
+class _WidgetHandleEventFilter(qt.QObject):
+    """Dedicated class to handle eventFilter callbacks"""
+    def __init__(self, parent):
+        super(_WidgetHandleEventFilter, self).__init__(parent=parent)
+        self._plot = weakref.ref(parent)
+
+    def getPlotWidget(self):
+        return self._plot()
+
+    def eventFilter(self, widgetHandle, event):
+        if event.type() == qt.QEvent.Leave:
+            plot = self.getPlotWidget()
+            plot._emitMouseLeft()
+            return True
+        return False
+
 
 class PlotWidget(qt.QMainWindow):
     """Qt Widget providing a 1D/2D plot.
@@ -372,6 +389,9 @@ class PlotWidget(qt.QMainWindow):
     It provides the menu which will be displayed.
     """
 
+    sigMouseLeft = qt.Signal()
+    """Signal emitted when the mouse move outside of the plot widget"""
+
     def __init__(self, parent=None, backend=None):
         self._autoreplot = False
         self._dirty = False
@@ -388,8 +408,12 @@ class PlotWidget(qt.QMainWindow):
         else:
             self.setWindowTitle('PlotWidget')
 
+        self.__eventFilter = _WidgetHandleEventFilter(self)
+        """Handle events from the WidgetHandler"""
+
         # Init the backend
         self._backend = self.__getBackendClass(backend)(self, self)
+        self._backend.installEventFilter(self.__eventFilter)
 
         self.setCallback()  # set _callback
 
@@ -592,12 +616,18 @@ class PlotWidget(qt.QMainWindow):
         for item in self.getItems():
             item._removeBackendRenderer(self._backend)
 
+        if self._backend is not None:
+            self._backend.removeEventFilter(self.__eventFilter)
+
         # Switch backend
         self._backend = backend
         widget = self._backend.getWidgetHandle()
         self.setCentralWidget(widget)
         if widget is None:
             _logger.info("PlotWidget backend does not support widget")
+
+        if self._backend is not None:
+            self._backend.installEventFilter(self.__eventFilter)
 
         # Mark as newly dirty
         self._dirty = False
@@ -3502,6 +3532,9 @@ class PlotWidget(qt.QMainWindow):
         if self._cursorInPlot:
             self._cursorInPlot = False
             self._eventHandler.handleEvent('leave')
+
+    def _emitMouseLeft(self):
+        self.sigMouseLeft.emit()
 
     # Interaction modes #
 
